a67e5e16ee097e25b22f3a9f63d02f92147f0ec7
Henry Hale <92443116+henryhale@users.noreply.github.com>
2025-10-13 01:02:14 +0300

    feat: improve data augmentation function

--- core/augmentation.py
+++ core/augmentation.py
@@ -7,7 +7,6 @@ from nltk.corpus import wordnet
 # >> nltk.download('wordnet')
 
 
-
 def synonym_augment(text_str, num_replacements=2):
     words = text_str.split()
     for _ in range(num_replacements):
@@ -20,12 +19,14 @@ def synonym_augment(text_str, num_replacements=2):
     return " ".join(words)
 
 
-def augment_data(df:pandas.DataFrame):
+def augment_data(df: pandas.DataFrame, variations=4):
     augmented = []
-    for i in range(len(df['input_variation'])):
+    for i in range(len(df["input_variation"])):
         text = df["input_variation"][i]
         label = df["output_class"][i]
-        augmented.append({text, label})
-        for _ in range(4):  # generate 4 variations per sample
+        augmented.append((text, label))
+        for _ in range(variations):  # generate n variations per sample
             augmented.append((synonym_augment(text), label))
-    return pandas.DataFrame(augmented)
+    ndf = pandas.DataFrame(augmented)
+    ndf.columns = ['text', 'label']
+    return ndf
